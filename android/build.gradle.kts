188db5da5af127eb61a0179130b3cfe39847eb12
Aaron Chan <develop@aeccue.com>
2023-09-25 17:32:16 +0900

    Created Android project for controller app

---
+++ android/build.gradle.kts
@@ -0,0 +1,11 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.1.0-alpha02")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+    }
+}
